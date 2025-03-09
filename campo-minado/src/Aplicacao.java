4dda15a7f0b65a56a9d07d8c1237faaa5404bcb4
Leonardo Borges Lopes <borgesleo780@gmail.com>
2025-03-09 08:43:42 -0300

    Adiciona lógica para funcionamento do jogo na classe TabuleiroConsole

--- campo-minado/src/Aplicacao.java
+++ campo-minado/src/Aplicacao.java
@@ -1,13 +1,10 @@
 import modelo.Tabuleiro;
+import visao.TabuleiroConsole;
 
 public class Aplicacao {
 	public static void main(String[] args) {
 		Tabuleiro tabuleiro = new Tabuleiro(6,6,6);
 		
-		tabuleiro.abrirCampo(3, 3);
-		tabuleiro.marcarCampo(4, 4);
-		tabuleiro.marcarCampo(4, 5);
-		
-		System.out.println(tabuleiro);
+		new TabuleiroConsole(tabuleiro);
 	}
 }
---
+++ campo-minado/src/visao/TabuleiroConsole.java
@@ -0,0 +1,79 @@
+package visao;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Scanner;
+
+import excecao.ExplosaoException;
+import excecao.SairException;
+import modelo.Tabuleiro;
+
+public class TabuleiroConsole {
+	private Tabuleiro tabuleiro;
+	private Scanner entrada = new Scanner(System.in);
+	
+	public TabuleiroConsole(Tabuleiro tabuleiro) {
+		 this.tabuleiro = tabuleiro;
+		 
+		 executarJogo();
+	}
+
+	private void executarJogo() {
+		try {
+			boolean continuar = true;
+			
+			while (continuar) {
+				cicloDoJogo();
+				
+				System.out.println("\nOutra partida? (S/n)");
+				String resposta = entrada.nextLine();
+				if ("n".equalsIgnoreCase(resposta)) {
+					continuar = false;
+				} else {
+					tabuleiro.reiniciar();
+				} 
+			}
+		} catch (SairException e) {
+			System.out.println("\nSaindo do jogo");
+		} finally {
+			entrada.close();
+		}
+		
+	}
+
+	private void cicloDoJogo() {
+		try {
+			while(!tabuleiro.objetivoAlcancado()) {
+				System.out.println(tabuleiro);
+				
+				String digitado = capturarValorDigitado("Digite (x,y): ");
+				
+				Iterator<Integer> xy = Arrays.stream(digitado.split(","))
+						.map(e -> Integer.parseInt(e.trim())).iterator();
+				
+				digitado = capturarValorDigitado("\n1-Abrir ou 2-(Des)Marcar ");
+				
+				if("1".equals(digitado)) {
+					tabuleiro.abrirCampo(xy.next(), xy.next());
+				} else if("2".equals(digitado)) {
+					tabuleiro.marcarCampo(xy.next(), xy.next());
+				}
+			}
+			
+			System.out.println("\nVocê ganhou!");
+		} catch (ExplosaoException e) {
+			System.out.println("\nVocê perdeu!");
+		}
+	}
+	
+	private String capturarValorDigitado(String texto) {
+		System.out.print(texto);
+		String digitado = entrada.nextLine();
+		
+		if("sair".equalsIgnoreCase(digitado)) {
+			throw new SairException();
+		}
+		
+		return digitado;
+	}
+}
